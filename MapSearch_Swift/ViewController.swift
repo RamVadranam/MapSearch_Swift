c5033e109b2bbeec5a7010bf0a81eb698477facf
Rama Chandra <ram2chitti@Rama-Chandras-MacBook.local>
2014-10-13 15:08:56 +0100

    Initial commit

--- MapSearch_Swift/ViewController.swift
+++ MapSearch_Swift/ViewController.swift
@@ -7,9 +7,76 @@
 //
 
 import UIKit
-
+import MapKit
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    func searchBarSearchButtonClicked(sender:UISearchBar)
+    {
+        self.searchBar.resignFirstResponder()
+        
+        var geocoder=CLGeocoder()
+        
+        geocoder.geocodeAddressString(searchBar.text, completionHandler:
+            { (placemarks, error) -> Void in
+                
+            if (error != nil) {
+                println("failed with error" + error.localizedDescription)
+                        return
+                    }
+
+            
+            var placemark:CLPlacemark=placemarks[0] as CLPlacemark
+           var location:CLLocationCoordinate2D=placemark.location.coordinate
+            
+            var annoation=MKPointAnnotation()
+            annoation.coordinate=location
+            annoation.title=self.searchBar.text
+            self.mapView.addAnnotation(annoation)
+            
+            var mr:MKMapRect=self.mapView.visibleMapRect
+            var pt:MKMapPoint=MKMapPointForCoordinate(annoation.coordinate)
+            mr.origin.x=pt.x-mr.size.width*0.5
+            mr.origin.y=pt.y-mr.size.height*0.25
+            
+            self.mapView.setVisibleMapRect(mr, animated: true)
+            
+        })
+        
+       
+       /* geocoder.geocodeAddressString(searchBar.text, completionHandler:
+            { (placeMarks:[]!, error:NSError!) -> Void in
+            
+                var placeMark:CLPlacemark=placeMarks[0] as CLPlacemark
+                var region:MKCoordinateRegion
+                var location:CLLocationCoordinate2D=placeMark.location.coordinate
+               
+                //region.center=placeMark.region
+                
+                var annoation=MKPointAnnotation()
+                annoation.coordinate=location
+                annoation.title=self.searchBar.text
+                self.mapView.addAnnotation(annoation)
+                
+                var mr:MKMapRect=self.mapView.visibleMapRect
+                var pt:MKMapPoint=MKMapPointForCoordinate(annoation.coordinate)
+                mr.origin.x=pt.x-mr.size.width*0.5
+                mr.origin.y=pt.y-mr.size.height*0.25
+                
+                self.mapView.setVisibleMapRect(mr, animated: true)
+                
+                
+                
+                
+                
+                
+                
+        })*/
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
